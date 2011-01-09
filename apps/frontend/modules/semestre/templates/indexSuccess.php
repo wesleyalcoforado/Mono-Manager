a4b6672a6d473bfd2cb3db078d59b8b6183c0322
Wesley Oliveira <wesleyjefferson@gmail.com>
2011-01-09 18:05:24 -0300

    remoção de extjs no semestre

--- apps/frontend/modules/semestre/templates/indexSuccess.php
+++ apps/frontend/modules/semestre/templates/indexSuccess.php
@@ -1,15 +1,10 @@
 <script type="text/javascript" >
-Ext.onReady(function(){
+/*Ext.onReady(function(){
     app.init();
-});
+});*/
 
 var app = {
     init: function(){
-        var grid = new Ext.ux.grid.TableGrid("listagem", {
-            stripeRows: true
-        });
-        grid.render();
-        
         app.createDatePicker("semestre_data_inicio_day", "semestre_data_inicio_month", "semestre_data_inicio_year");
         app.createDatePicker("semestre_data_proposta_day", "semestre_data_proposta_month", "semestre_data_proposta_year");
         app.createDatePicker("semestre_data_apresentacao_day", "semestre_data_apresentacao_month", "semestre_data_apresentacao_year");
@@ -28,8 +23,6 @@ var app = {
         datePickerController.createDatePicker(opts);    
     }
 }
-
-
 </script>
 
 <?php
