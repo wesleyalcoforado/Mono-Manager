89842b364ea43b98664fefbb63df5c9e330ee8f2
Wesley Oliveira <wesleyjefferson@gmail.com>
2011-01-25 07:39:04 -0300

    tradução do form de login

--- apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
+++ apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,6 +1,17 @@
 <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
-  <table>
-    <?php echo $form ?>
-    <tr><td colspan="2" align="center"><input type="submit" value="Entrar" /></td></tr>
-  </table>
+  <?php
+    echo $form['username']->renderLabel('Login')
+       . $form['username']->render();
+  ?> <br/>
+  <?php
+    echo $form['password']->renderLabel('Senha')
+       . $form['password']->render();
+  ?> <br/>
+  <?php
+    echo $form['remember']->renderLabel('Lembrar de mim?')
+       . $form['remember']->render();
+  ?> <br/>
+
+  <input type="submit" value="Entrar" />
+
 </form>
