73b479c0480dc99630e26630443056993917d4ea
Wesley Oliveira <wesleyjefferson@gmail.com>
2011-02-06 18:08:49 -0300

    refactoring no controle de datas

--- apps/frontend/modules/semestre/templates/indexSuccess.php
+++ apps/frontend/modules/semestre/templates/indexSuccess.php
@@ -1,32 +1,12 @@
 <?php use_helper('App'); ?>
 <script type="text/javascript" >
-var app = {
-  init: function(){
-    app.createDatePicker("semestre_data_inicio_day", "semestre_data_inicio_month", "semestre_data_inicio_year");
-    app.createDatePicker("semestre_data_proposta_day", "semestre_data_proposta_month", "semestre_data_proposta_year");
-    app.createDatePicker("semestre_data_apresentacao_day", "semestre_data_apresentacao_month", "semestre_data_apresentacao_year");
-  },
-
-  createDatePicker: function(day, month, year){
-    var elements = new Object();
-    elements[year] = 'Y';
-    elements[month] = 'n';
-    elements[day] = 'j';
-
-    var opts = {
-        formElements: elements
-    };
-
-    datePickerController.createDatePicker(opts);
-  }
+var init = app.init;
+app.init = function(){
+  init();
+  app.createDatePicker("semestre_data_inicio_day", "semestre_data_inicio_month", "semestre_data_inicio_year");
+  app.createDatePicker("semestre_data_proposta_day", "semestre_data_proposta_month", "semestre_data_proposta_year");
+  app.createDatePicker("semestre_data_apresentacao_day", "semestre_data_apresentacao_month", "semestre_data_apresentacao_year");
 }
-
-
-$(function() {
-  app.init();
-});
-
-
 </script>
 
 <h1>Semestres</h1>
--- web/js/app.js
+++ web/js/app.js
@@ -1,11 +1,33 @@
+var app = {
+  init: function(){
+    app.stylishInputs();
+  },
+
+  createDatePicker: function(day, month, year){
+    var elements = new Object();
+    elements[year] = 'Y';
+    elements[month] = 'n';
+    elements[day] = 'j';
+
+    var opts = {
+        formElements: elements
+    };
+
+    datePickerController.createDatePicker(opts);
+  },
+
+  stylishInputs: function(){
+    $("input[type=text], input[type=password], textarea, #projeto_professor_id").addClass("width400");
+
+    $("input[type=text], input[type=password], textarea, select").addClass("idle");
+    $("input[type=text], input[type=password], textarea, select").focus(function(){
+      $(this).addClass("activeField").removeClass("idle");
+    }).blur(function(){
+      $(this).removeClass("activeField").addClass("idle");
+    });
+  }
+}
+
 $(document).ready(function(){
-  $("input[type=text], input[type=password], textarea, #projeto_professor_id").addClass("width400");
-
-  $("input[type=text], input[type=password], textarea, select").addClass("idle");
-  $("input[type=text], input[type=password], textarea, select").focus(function(){
-    $(this).addClass("activeField").removeClass("idle");
-  }).blur(function(){
-    $(this).removeClass("activeField").addClass("idle");
-  });
+  app.init();
 });
-
