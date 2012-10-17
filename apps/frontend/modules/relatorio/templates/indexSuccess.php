496116f3c94c141e677ecd498dbbd67e9b02649b
Wesley Jefferson <wesleyjefferson@gmail.com>
2012-10-16 23:28:25 -0300

    correcao no layout dos links

--- apps/frontend/modules/relatorio/templates/indexSuccess.php
+++ apps/frontend/modules/relatorio/templates/indexSuccess.php
@@ -1,34 +1,29 @@
 <h1>Relatórios</h1>
 
-<ul id="icons_table">
-  <li>
-    <?php
-      echo link_to(image_tag('icons/project_status') . "<br/>Status dos projetos", 'relatorio/status');
-    ?>
-  </li>
-  <li>
-    <?php
-      echo link_to(image_tag('icons/concluded_projects') . "<br/>Projetos defendidos<br/>por semestre", 'relatorio/concluidos');
-    ?>
-  </li>
-  <li>
-    <?php
-      echo link_to(image_tag('icons/document') . "<br/>Ata", 'relatorio/documentos/tipo/ata');
-    ?>
-  </li>
-  <li>
-    <?php
-      echo link_to(image_tag('icons/document') . "<br/>Declaração Orientador", 'relatorio/documentos/tipo/orientador');
-    ?>
-  </li>
-  <li>
-    <?php
-      echo link_to(image_tag('icons/document') . "<br/>Declaração Banca", 'relatorio/documentos/tipo/banca');
-    ?>
-  </li>	
-  <li>
-    <?php
-      echo link_to(image_tag('icons/document') . "<br/>Ficha de avaliação", 'relatorio/documentos/tipo/ficha');
-    ?>
-  </li>			
-</ul>
+<table>
+	<tr>
+	  <td>
+			<?php echo link_to(image_tag('icons/project_status') . "<br/>Status dos projetos", 'relatorio/status'); ?>
+	  </td>
+		<td>
+			<?php echo link_to(image_tag('icons/concluded_projects') . "<br/>Projetos defendidos<br/>por semestre", 'relatorio/concluidos'); ?>
+		</td>
+		<td>
+			<?php echo link_to(image_tag('icons/document') . "<br/>Ata", 'relatorio/documentos?tipo=ata'); ?>
+		</td>
+		<td>
+			<?php echo link_to(image_tag('icons/document') . "<br/>Declaração Orientador", 'relatorio/documentos?tipo=orientador'); ?>
+		</td>
+  </tr>
+	<tr>
+		<td>
+			<?php echo link_to(image_tag('icons/document') . "<br/>Declaração Banca", 'relatorio/documentos?tipo=banca'); ?>
+		</td>
+		<td>
+			<?php echo link_to(image_tag('icons/document') . "<br/>Ficha de avaliação", 'relatorio/documentos?tipo=ficha'); ?>
+		</td>
+		<td></td><td></td>
+	</tr>
+</table>
+
+
